55cc182a130a8d046388ddfd09ae31089fddafb5
nchen0 <nicky.chen0@gmail.com>
2019-07-21 09:33:09 -0400

    course

---
+++ src/com/hibernate/entity/Course.java
@@ -0,0 +1,56 @@
+package com.hibernate.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+    private Instructor instructor;
+
+    public Course() {
+    }
+
+    public Course(String title, Instructor instructor) {
+        this.title = title;
+        this.instructor = instructor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", instructor=" + instructor +
+                '}';
+    }
+}
